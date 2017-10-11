4746bc783d02117a0b614225bc6d041db1da0abc
sjumilli <sjur.millidahl@finn.no>
2017-10-11 12:18:46 +0200

    Obfuscated part 4

--- src/main/java/workshop/part4/HttpRoutes.java
+++ src/main/java/workshop/part4/HttpRoutes.java
@@ -1,20 +1,14 @@
 package workshop.part4;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
 import akka.actor.ActorRef;
-import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.StatusCodes;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
-import akka.pattern.Patterns;
-import scala.compat.java8.FutureConverters;
-import scala.concurrent.Future;
-import scala.reflect.ClassTag$;
 import workshop.common.ad.Ad;
 import workshop.part1.Verdict;
-
-import static akka.http.javadsl.server.PathMatchers.integerSegment;
 
 class HttpRoutes extends AllDirectives {
 
@@ -26,15 +20,11 @@ class HttpRoutes extends AllDirectives {
         this.cache = cache;
     }
 
-    /**
-     * Dont forget to register your route here - otherwise you will be pondering 404s!
-     */
     public Route registerRoutes() {
         return route(
                 root(),
                 verdict(),
-                evaluate(),
-                error()
+                evaluate()
         );
     }
 
@@ -43,58 +33,23 @@ class HttpRoutes extends AllDirectives {
                 path("", () -> complete(StatusCodes.NOT_FOUND)));
     }
 
-    private Route error() {
-        return post(() ->
-                path("evaluate", () ->
-                        entity(Jackson.unmarshaller(Ad.class), ad ->
-                                completeOK(new Verdict(String.valueOf(ad.getAdId()), Verdict.VerdictType.FAILURE), Jackson.marshaller()))));
-    }
-
-    private Route evaluate() {
-        return post(() ->
-                path("evaluate", () ->
-                        entity(Jackson.unmarshaller(Ad.class), ad ->
-                                cache.get(ad)
-                                .map(a -> verdict())
-                                .getOrElse(() -> completeOrRecoverWith(
-                                        () -> doVetting(ad),
-                                        Jackson.marshaller(),
-                                        t -> error()))
-                        ))
-        );
-    }
-
+    // GET to /verdict/<numeric id> should result in the verdict of the ad with that adId e.g.
+    // GET /verdict/2 -> {"id": "2", "value": "GOOD"}
     private Route verdict() {
-        return get(() ->
-                pathPrefix("evaluate", () ->
-                        path(integerSegment(), adId -> {
-                            return cache.get(adId)
-                                    .map(verdict -> complete(StatusCodes.OK, verdict, Jackson.marshaller()))
-                                    .getOrElse(() -> complete(StatusCodes.NOT_FOUND, String.format("No such vetting: %d", adId)));
-                                }
-                        )));
+        return root();//todo
     }
 
+    // POST to /evaluate with a json-representation of an Ad.class :
+    // {"adId": "2", "userId": "99999", "title": "fin sofa selges", "description": "brukt noen år da, si"}
+    // should result in a json representation of Verdict.class :
+    // {"id": "2", "value": "GOOD"}
+    private Route evaluate() {
+        return root(); //todo
+    }
+
+    // do actual vetting using a VettingActor and Futures here
     private CompletionStage<Verdict> doVetting(final Ad ad) {
-
-        String verdictId = String.valueOf(ad.getAdId());
-
-        Future<Verdict.VerdictType> actorVerdict = Patterns.ask(vettingActor, ad, 1000)
-                .mapTo(ClassTag$.MODULE$.apply(Verdict.VerdictType.class));
-
-        CompletionStage<Verdict.VerdictType> vettingVerdict = FutureConverters.toJava(actorVerdict);
-
-        return vettingVerdict.whenCompleteAsync((Verdict.VerdictType vt, Throwable error) -> {
-            if (error != null) {
-                vettingVerdict.thenRunAsync(() -> new Verdict(verdictId, Verdict.VerdictType.FAILURE));
-            } else {
-                vettingVerdict.thenRunAsync(() -> {
-                    Verdict verdict = new Verdict(verdictId, vt);
-                    cache.put(ad, verdict);
-                });
-            }
-        }).thenApply(vt -> new Verdict(verdictId, vt));
-
+        return CompletableFuture.supplyAsync(() -> new Verdict("todo", Verdict.VerdictType.PENDING));
     }
 
 }
